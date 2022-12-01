589f1d06b9ab5ad12f6c06763886b1bf4ff4fe01
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-01 13:19:13 -0500

    moves go day1 part1 solution into separate function

--- 2022/lang/go/internal/solution/day-01.go
+++ 2022/lang/go/internal/solution/day-01.go
@@ -10,17 +10,19 @@ import (
 
 func Day01() {
 	fmt.Println("- Day 01")
-	fmt.Print("  - Part 1: ")
 	file, err := os.Open("input/day-01.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	part1(scanner)
+}
 
+func part1(scanner *bufio.Scanner) {
+	fmt.Print("  - Part 1: ")
 	currentCalorieSum := 0
 	largestCalorieSum := 0
-
-	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
