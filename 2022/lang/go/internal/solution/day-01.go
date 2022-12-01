f825addb0b78d8de6604ec6ec39a5e6abdc344bb
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-01 13:37:21 -0500

    adds go solution for 2022 day 1 part 2

--- 2022/lang/go/internal/solution/day-01.go
+++ 2022/lang/go/internal/solution/day-01.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -15,8 +16,17 @@ func Day01() {
 		log.Fatal(err)
 	}
 	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 	part1(scanner)
+
+	file, err = os.Open("input/day-01.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner = bufio.NewScanner(file)
+	part2(scanner)
 }
 
 func part1(scanner *bufio.Scanner) {
@@ -42,4 +52,27 @@ func part1(scanner *bufio.Scanner) {
 		log.Fatal(err)
 	}
 	fmt.Print("Most calories is ", largestCalorieSum, "\n")
+}
+
+func part2(scanner *bufio.Scanner) {
+	fmt.Print("  - Part 2: ")
+	calorieSums := []int{}
+	currentCalorieSum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			calorieSums = append(calorieSums, currentCalorieSum)
+			currentCalorieSum = 0
+		} else {
+			calories, err := strconv.Atoi(line)
+			if err != nil {
+				log.Fatal(err)
+			}
+			currentCalorieSum += calories
+		}
+	}
+	sort.Ints(calorieSums)
+	elfCount := len(calorieSums)
+	topThree := calorieSums[elfCount-1] + calorieSums[elfCount-2] + calorieSums[elfCount-3]
+	fmt.Print("Sum of calories for top 3 elves is ", topThree, "\n")
 }
