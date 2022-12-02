d9331f1551471d033324130161de2f56a99c0b94
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-02 13:40:44 -0500

    removes nim from the nix-shell because not all of the tools are installing correctly

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) {};
 in
   with pkgs;
   mkShell {
@@ -13,7 +14,9 @@ in
       rust-bin.stable."1.64.0".default
       rust-analyzer
       # Nim
-      nim
+      ## Nimble fails does not appear to work when installed with nix-shell, so
+      ## relying on a non-nix installation for the time being.
+      # nim
       nimlsp
     ];
   }
