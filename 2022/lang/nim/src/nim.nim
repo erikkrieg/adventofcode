ed35163bcfe563aca1be3f7b5dc23f1e60049a6e
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-03 10:29:52 -0500

    solves day 1, 2022 with nim

--- 2022/lang/nim/src/nim.nim
+++ 2022/lang/nim/src/nim.nim
@@ -1,5 +1,27 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+from std/strutils import parseInt
+from std/algorithm import sort
+from std/math import sum
+
+proc day01() =
+  echo("- Day 01")
+  let input = open("input/day-01.txt")
+  defer: input.close()
+
+  var sums = @[0]
+  var line : string
+  while input.readLine(line):
+    case line:
+      of "":
+        sums.add(0)
+      else:
+        sums[^1] += parseInt(line)
+  sums.sort()
+  sums = sums[^3..^1]
+
+  echo("  - Part 1: Most calories is ", sums.max())
+  echo("  - Part 2: Sum of calories for top 3 elves is ", sums.sum())
 
 when isMainModule:
-  echo("Hello, World!")
+  echo("Advent of Code 2022")
+  day01()
+
