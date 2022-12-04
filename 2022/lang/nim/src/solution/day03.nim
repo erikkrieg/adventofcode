19eb0d6ac8d2e2763eaeac768d360fc1d0c34ee5
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-03 23:33:11 -0500

    solves 2022 day 3 part 2 with nim

--- 2022/lang/nim/src/solution/day03.nim
+++ 2022/lang/nim/src/solution/day03.nim
@@ -1,22 +1,48 @@
 import std/sequtils
 
-proc solve*() =
-  echo("- Day 03")
-  let input = open("input/day-03.txt")
+proc score(c: char): int =
+    let c = int(c)
+    if c >= 97:
+      # a-z is scored 1-26
+      return c - 96 
+    else:
+      # A-Z is scored 27-52
+      return c - 38
 
+proc partOne() =
+  let input = open("input/day-03.txt")
   var
     line: string
     prioritySum: int
-
   while input.readLine(line):
     let split = toSeq(line.items).distribute(2)
-    let match = filter(split[0], proc(s: char): bool = split[1].contains(s))
-    let c = int(match[0])
-    if c >= 97:
-      # a-z is scored 1-26
-      prioritySum += c - 96 
-    else:
-      # A-Z is scored 27-52
-      prioritySum += c - 38
-
+    let match = filter(split[0], proc(c: char): bool = split[1].contains(c))
+    prioritySum += score(match[0])
   echo("  - Part 1: ", prioritySum)
+
+proc partTwo() =
+  let input = open("input/day-03.txt")
+  var
+    line: string
+    prioritySum: int
+    i = 0
+    group: seq[seq[char]]
+  while input.readLine(line):
+    group.add(deduplicate(toSeq(line.items)))
+    i += 1
+    if i == 3:
+      let match = filter(
+        group[0],
+        proc(c: char): bool = group[1].contains(c) and group[2].contains(c)
+      )
+      prioritySum += score(match[0])
+      i = 0
+      group = @[]
+  echo("  - Part 1: ", prioritySum)
+
+
+proc solve*() =
+  echo("- Day 03")
+  partOne()
+  partTwo()
+
