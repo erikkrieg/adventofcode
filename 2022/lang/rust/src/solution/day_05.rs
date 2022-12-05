98b34556d000daafd1750de76417c4c529e11dd0
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-05 02:06:35 -0500

    solves 2022 day 5 part 2 with rust

--- 2022/lang/rust/src/solution/day_05.rs
+++ 2022/lang/rust/src/solution/day_05.rs
@@ -1,9 +1,18 @@
 use std::{fs, io};
 
-// This isn't very necessary, I was just interested in trying to use some different
-// language features.
+// Using type alias and a custom trait just for practice with more language features
 type Stack = Vec<char>;
 type Stacks = Vec<Stack>;
+
+trait Top {
+    fn top(&self) -> String;
+}
+
+impl Top for Stacks {
+    fn top(&self) -> String {
+        self.iter().filter_map(|s| s.last()).collect::<String>()
+    }
+}
 
 fn parse_stacks(raw_stacks: &str) -> Stacks {
     let mut stacks: Stacks = Vec::new();
@@ -42,13 +51,14 @@ pub fn solve() -> io::Result<()> {
     let splits: Vec<&str> = input.split("\n\n").collect();
     let stacks = parse_stacks(splits[0]);
     let moves = parse_moves(splits[1]);
-    part_one(stacks, &moves);
+    part_one(&stacks, &moves);
+    part_two(&stacks, &moves);
 
     Ok(())
 }
 
-fn part_one(stacks: Stacks, moves: &[Vec<usize>]) {
-    let mut stacks = stacks;
+fn part_one(stacks: &Stacks, moves: &[Vec<usize>]) {
+    let mut stacks = stacks.clone();
     moves.iter().for_each(|m| {
         for _ in 0..m[0] {
             if let Some(last) = stacks[m[1] - 1].pop() {
@@ -56,7 +66,15 @@ fn part_one(stacks: Stacks, moves: &[Vec<usize>]) {
             }
         }
     });
+    println!("  - Part 1: {}", stacks.top());
+}
 
-    let top = stacks.iter().filter_map(|s| s.last()).collect::<String>();
-    println!("  - Part 1: {top}");
+fn part_two(stacks: &Stacks, moves: &[Vec<usize>]) {
+    let mut stacks = stacks.clone();
+    moves.iter().for_each(|m| {
+        let split_at = stacks[m[1] - 1].len() - m[0];
+        let mut move_stacks = stacks[m[1] - 1].split_off(split_at);
+        stacks[m[2] - 1].append(&mut move_stacks);
+    });
+    println!("  - Part 2: {}", stacks.top());
 }
