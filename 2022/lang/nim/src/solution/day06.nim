c280925ab08d8d305b985bf22a563772751a46a7
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-06 13:58:24 -0500

    solves 2022 day 6 part 2 with nim

--- 2022/lang/nim/src/solution/day06.nim
+++ 2022/lang/nim/src/solution/day06.nim
@@ -1,29 +1,37 @@
 from std/sequtils import deduplicate
 
-proc shiftMarker(arr: array[4, char], insert: char): array[4, char] =
-  for a in 1..3:
-    result[a-1] = arr[a]
-  result[3] = insert
+# The crux of this puzzle for me was figuring out how I could write procedures
+# that accept arrays of different lengths. I was using arrays because it appeared
+# to be the data type that should be used with `readChars`, which I wanted to
+# try using.
+# I was worried that openArray might not behave correctly with readChars, but
+# it fortunately did respect the array's capacity. Unfortunately I made a bad
+# assumption that _it would not_ and took a fruitless detour into macros XD
 
-proc endOfMarkerIndex(): int =
+proc shiftMarker(arr: var openArray[char], insert: char) =
+  for a in (arr.low + 1) .. arr.high():
+    arr[a-1] = arr[a]
+  arr[arr.high()] = insert
+
+proc findMarker(marker: var openArray[char]): int =
   let input = open("input/day-06.txt")
   defer: input.close()
   var
-    iterations = 4
-    potentialMarker: array[4, char]
+    iterations = marker.len()
     cursor: array[1, char]
-  discard readChars(input, potentialMarker)
+  discard readChars(input, marker)
   while readChars(input, cursor) > 0:
-    if deduplicate(potentialMarker).len() == 4:
+    if deduplicate(marker).len() == marker.len():
       break
-    potentialMarker = shiftMarker(potentialMarker, cursor[0])
+    shiftMarker(marker, cursor[0])
     iterations += 1
   return iterations
 
-proc partOne() =
-  echo("  - Part 1: ", endOfMarkerIndex())
-
 proc solve*() =
   echo("- Day 06")
-  partOne()
+  var
+    partOneArr: array[4, char]
+    partTwoArr: array[14, char]
+  echo("  - Part 1: ", findMarker(partOneArr))
+  echo("  - Part 2: ", findMarker(partTwoArr))
 
