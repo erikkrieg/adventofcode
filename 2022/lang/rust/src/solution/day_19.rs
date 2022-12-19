207dc6f816d75d703bdf35098ecdea2a02950c48
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-19 12:28:56 -0500

    solves 2022 day 19 part 2 in rust (with luck)

--- 2022/lang/rust/src/solution/day_19.rs
+++ 2022/lang/rust/src/solution/day_19.rs
@@ -113,13 +113,18 @@ enum Action {
 fn simulate(bp: &Blueprint, duration: usize) -> usize {
     let mut sims = VecDeque::from([SimulationState::new()]);
     let mut max_geode_count = 0;
-
     while let Some(mut state) = sims.pop_front() {
-        //println!("{state:?}");
         if state.minutes_passed == duration
             || (state.minutes_passed > 18 && state.obsidian_robots < 1)
             || (state.minutes_passed > 23 && state.geode_robots < 1)
+            // Pruning sims that have waited too much. Values tuned with
+            // trial and error (mostly error).
+            || (state.ore_robots + state.clay_robots + state.obsidian_robots + state.geode_robots) + 1 < state.minutes_passed.div_floor(2)
         {
+            // Useful to compare with the sample from the puzzle description
+            //if state.geode_count > max_geode_count {
+            //    println!("{state:?}");
+            //}
             max_geode_count = max_geode_count.max(state.geode_count);
             continue;
         }
@@ -129,17 +134,18 @@ fn simulate(bp: &Blueprint, duration: usize) -> usize {
         if state.ore_count >= bp.geode_robot.ore && state.obsidian_count >= bp.geode_robot.obsidian
         {
             available_actions.push(BuyGeodeBot);
+        } else if state.ore_count >= bp.obsidian_robot.ore
+            && state.clay_count >= bp.obsidian_robot.clay
+        {
+            available_actions.push(BuyObsidianBot);
+            // testing to see if waiting 50% of the time helps
+            available_actions.push(Wait);
         } else {
             if state.ore_count >= bp.ore_robot.ore && state.ore_robots < bp.max_ore_cost() {
                 available_actions.push(BuyOreBot);
             }
             if state.ore_count >= bp.clay_robot.ore && state.clay_robots < bp.max_clay_cost() {
                 available_actions.push(BuyClayBot);
-            }
-            if state.ore_count >= bp.obsidian_robot.ore
-                && state.clay_count >= bp.obsidian_robot.clay
-            {
-                available_actions.push(BuyObsidianBot);
             }
             available_actions.push(Wait);
         }
@@ -183,8 +189,7 @@ fn simulate(bp: &Blueprint, duration: usize) -> usize {
             }
         });
     }
-    //println!("sim: {state:#?}");
-    println!("{max_geode_count}");
+    //println!("{max_geode_count}");
     max_geode_count
 }
 
@@ -194,6 +199,7 @@ pub fn solve() -> io::Result<()> {
     //let input = File::open("input/sample-19.txt")?;
     let blueprints = parse_blueprints(BufReader::new(input));
     part_one(&blueprints);
+    part_two(&blueprints);
     Ok(())
 }
 
@@ -205,4 +211,19 @@ fn part_one(blueprints: &[Blueprint]) {
         .map(|(i, b)| (i + 1) * simulate(b, duration))
         .sum();
     println!("  - Part 1: {quality_level_sum}");
+}
+
+// TODO: The simulation at time of this commit doesn't actually solve the sample input.
+// It gets 54 instead of 56 for the first blueprint. After a bit of tweaks I did
+// feel it was worth a shot to see if I might get lucky with my sims and find the
+// best values and sure enough I was. I want to keep this mess as a reference, but
+// would like to fix and refactor heavily.
+fn part_two(blueprints: &[Blueprint]) {
+    let duration = 32;
+    let product: usize = blueprints
+        .iter()
+        .take(3)
+        .map(|b| simulate(b, duration))
+        .product();
+    println!("  - Part 2: {product}");
 }
