e8ef4d7668a5078df5d4647599cedd9a4e6655a3
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-12-19 03:18:57 -0500

    badly solves 2022 day 19 part 1 in rust

--- 2022/lang/rust/src/solution/day_19.rs
+++ 2022/lang/rust/src/solution/day_19.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::VecDeque,
     fs::File,
     io::{self, BufRead, BufReader},
 };
@@ -77,7 +78,7 @@ fn parse_blueprints(reader: BufReader<File>) -> Vec<Blueprint> {
         .collect()
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 struct SimulationState {
     ore_robots: usize,
     clay_robots: usize,
@@ -110,60 +111,87 @@ enum Action {
 // TODO: The simulation needs to test different combinations.
 // Consider creating a RecursiveSimulation.
 fn simulate(bp: &Blueprint, duration: usize) -> usize {
-    let mut state = SimulationState::new();
-    use Action::*;
-    while state.minutes_passed < duration {
-        let mut available_actions = Vec::new();
-        if state.ore_count >= bp.ore_robot.ore && state.ore_robots < bp.max_ore_cost() {
-            available_actions.push(BuyOreBot);
-        }
-        if state.ore_count >= bp.clay_robot.ore
-            && state.clay_robots < bp.max_clay_cost().div_floor(4).min(4)
+    let mut sims = VecDeque::from([SimulationState::new()]);
+    let mut max_geode_count = 0;
+
+    while let Some(mut state) = sims.pop_front() {
+        //println!("{state:?}");
+        if state.minutes_passed == duration
+            || (state.minutes_passed > 18 && state.obsidian_robots < 1)
+            || (state.minutes_passed > 23 && state.geode_robots < 1)
         {
-            available_actions.push(BuyClayBot);
+            max_geode_count = max_geode_count.max(state.geode_count);
+            continue;
         }
-        if state.ore_count >= bp.obsidian_robot.ore && state.clay_count >= bp.obsidian_robot.clay {
-            available_actions.push(BuyObsidianBot);
-        }
+        // Determine possible actions
+        use Action::*;
+        let mut available_actions = Vec::new();
         if state.ore_count >= bp.geode_robot.ore && state.obsidian_count >= bp.geode_robot.obsidian
         {
             available_actions.push(BuyGeodeBot);
+        } else {
+            if state.ore_count >= bp.ore_robot.ore && state.ore_robots < bp.max_ore_cost() {
+                available_actions.push(BuyOreBot);
+            }
+            if state.ore_count >= bp.clay_robot.ore && state.clay_robots < bp.max_clay_cost() {
+                available_actions.push(BuyClayBot);
+            }
+            if state.ore_count >= bp.obsidian_robot.ore
+                && state.clay_count >= bp.obsidian_robot.clay
+            {
+                available_actions.push(BuyObsidianBot);
+            }
+            available_actions.push(Wait);
         }
+
+        // update state
         state.ore_count += state.ore_robots;
         state.clay_count += state.clay_robots;
         state.obsidian_count += state.obsidian_robots;
         state.geode_count += state.geode_robots;
-        match available_actions.pop().unwrap_or(Wait) {
+        state.minutes_passed += 1;
+
+        available_actions.iter().for_each(|a| match a {
             BuyOreBot => {
+                let mut state = state.clone();
                 state.ore_robots += 1;
                 state.ore_count -= bp.ore_robot.ore;
+                sims.push_back(state);
             }
             BuyClayBot => {
+                let mut state = state.clone();
                 state.clay_robots += 1;
                 state.ore_count -= bp.clay_robot.ore;
+                sims.push_back(state);
             }
             BuyObsidianBot => {
+                let mut state = state.clone();
                 state.obsidian_robots += 1;
                 state.ore_count -= bp.obsidian_robot.ore;
                 state.clay_count -= bp.obsidian_robot.clay;
+                sims.push_back(state);
             }
             BuyGeodeBot => {
+                let mut state = state.clone();
                 state.geode_robots += 1;
                 state.ore_count -= bp.geode_robot.ore;
                 state.obsidian_count -= bp.geode_robot.obsidian;
+                sims.push_back(state);
             }
-            Wait => (),
-        }
-        state.minutes_passed += 1;
+            Wait => {
+                sims.push_back(state.clone());
+            }
+        });
     }
-    println!("sim: {state:#?}");
-    state.geode_count
+    //println!("sim: {state:#?}");
+    println!("{max_geode_count}");
+    max_geode_count
 }
 
 pub fn solve() -> io::Result<()> {
     println!("- Day 19:");
     let input = File::open("input/day-19.txt")?;
-    let input = File::open("input/sample-19.txt")?;
+    //let input = File::open("input/sample-19.txt")?;
     let blueprints = parse_blueprints(BufReader::new(input));
     part_one(&blueprints);
     Ok(())
