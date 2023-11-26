a702074fca929975af38a43bc06fc3b13d1cb3ae
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-11-25 22:58:39 -0500

    sets up dev shell for 2023

---
+++ 2023/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Declarative local dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/23.05";
+    unstable-nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { flake-utils, nixpkgs, unstable-nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        stable = import nixpkgs { inherit system; };
+        unstable = import unstable-nixpkgs { inherit system; };
+      in
+      with stable; {
+        devShell = mkShell {
+          buildInputs = [
+            go
+            gopls
+            just
+          ];
+          shellHook = ''
+            # Keep Go cache and module files in the project
+            export GOPATH="$(pwd)/.go"
+          '';
+        };
+      });
+}
