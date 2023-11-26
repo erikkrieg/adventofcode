717a7e3b32af55b977915568a27b4b8025578ba5
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-11-26 00:42:18 -0500

    2023: adds temp examples of using inputs pkg in go

--- 2023/languages/go/main.go
+++ 2023/languages/go/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/erikkrieg/adventofcode/2023/pkg/input"
+)
 
 func main() {
 	fmt.Println("AOC 2023")
+
+	// Examples of importing inputs as varied data structures.
+	chars := input.Chars("day-1")
+	fmt.Printf("Chars: %+v\n", chars)
+
+	lines := input.Lines("day-1")
+	fmt.Printf("Lines: %+v\n", lines)
+
+	linesChars := input.LinesChars("day-1")
+	fmt.Printf("Chars per line: %+v\n", linesChars)
 }
