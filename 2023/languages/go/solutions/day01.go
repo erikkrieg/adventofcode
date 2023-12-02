43a7b89fd22989290b21b805a210ea8f727730df
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-12-02 10:49:06 -0500

    2023: day 1 - refactor solution and include test inputs

--- 2023/languages/go/solutions/day01.go
+++ 2023/languages/go/solutions/day01.go
@@ -1,5 +1,3 @@
-// I'd like to refactor this so that I scan from the beginning and then end
-// of a line until a digit is found (close to my original part 1 solution).
 package solutions
 
 import (
@@ -9,76 +7,70 @@ import (
 	"github.com/erikkrieg/adventofcode/2023/pkg/input"
 )
 
+var day1Part1Test = []string{
+	"1abc2",
+	"pqr3stu8vwx",
+	"a1b2c3d4e5f",
+	"treb7uchet",
+}
+
+var day1Part2Test = []string{
+	"two1nine",
+	"eightwothree",
+	"abcone2threexyz",
+	"xtwone3four",
+	"4nineeightseven2",
+	"zoneight234",
+	"7pqrstsixteen",
+}
+
 func init() {
 	puzzleSolutions[0] = day1Solution
 }
 
 func day1Solution() {
 	fmt.Println("Day 1")
-	day1Part1(false)
-	day1Part2(false)
-}
-
-func day1Part1(test bool) {
-	fmt.Print(" Part 1: ")
-	documents := input.Lines("day-1")
-	if test {
-		documents = []string{
-			"1abc2",
-			"pqr3stu8vwx",
-			"a1b2c3d4e5f",
-			"treb7uchet",
-		}
-	}
-	digits := []string{
-		"1", "2", "3", "4", "5", "6", "7", "8", "9",
-	}
-	calibartion := trebuchet(documents, digits)
-	fmt.Println(calibartion)
-}
-
-func day1Part2(test bool) {
-	fmt.Print(" Part 2: ")
-	documents := input.Lines("day-1")
-	if test {
-		documents = []string{
-			"two1nine",
-			"eightwothree",
-			"abcone2threexyz",
-			"xtwone3four",
-			"4nineeightseven2",
-			"zoneight234",
-			"7pqrstsixteen",
-		}
-	}
 	digits := []string{
 		"1", "2", "3", "4", "5", "6", "7", "8", "9",
 		"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 	}
-	calibartion := trebuchet(documents, digits)
-	fmt.Println(calibartion)
+	fmt.Printf(" Part 1: %d\n", day1BothParts(digits[:9], &day1Part1Test))
+	fmt.Printf(" Part 2: %d\n", day1BothParts(digits, &day1Part2Test))
 }
 
-func trebuchet(documents []string, digits []string) int {
+func day1BothParts(digits []string, test *[]string) int {
+	documents := input.Lines("day-1")
+	if useTestInput {
+		documents = *test
+	}
+	return tre(documents, digits)
+}
+
+func tre(documents []string, digits []string) int {
 	sum := 0
 	for _, doc := range documents {
-		first := len(doc)
-		last := -1
-		nums := [2]int{}
-		for v, d := range digits {
-			i := strings.Index(doc, d)
-			num := v%9 + 1
-			if i > -1 && i < first {
-				first = i
-				nums[0] = num
-			}
-			i = strings.LastIndex(doc, d)
-			if i > last {
-				last = i
-				nums[1] = num
-			}
-		}
-		sum += nums[0]*10 + nums[1]
+		sum += findFirstDigit(doc, digits)*10 + findLastDigit(doc, digits)
 	}
 	return sum
+}
+
+func findFirstDigit(doc string, digits []string) int {
+	for i := 0; i < len(doc); i++ {
+		for v, d := range digits {
+			if strings.Contains(doc[:i+1], d) {
+				return v%9 + 1
+			}
+		}
+	}
+	return 0
+}
+func findLastDigit(doc string, digits []string) int {
+	for i := len(doc) - 1; i >= 0; i-- {
+		for v, d := range digits {
+			if strings.Contains(doc[i:], d) {
+				return v%9 + 1
+			}
+		}
+	}
+	return 0
 }
