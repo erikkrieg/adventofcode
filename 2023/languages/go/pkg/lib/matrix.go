8fbb9ce9c67cbfae9a0dcdeb85f18948c0822ae7
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-12-03 19:12:08 -0500

    2023: start work on matrix lib types and methods

---
+++ 2023/languages/go/pkg/lib/matrix.go
@@ -0,0 +1,35 @@
+package lib
+
+type Point struct {
+	X, Y int
+}
+
+// Edge methods are designed around vertical and horizontal edges. Diagonal
+// points will not be able to use all Edge methods.
+type Edge struct {
+	Start Point
+	End   Point
+}
+
+func (e Edge) Len() int {
+	deltaX := Abs(e.Start.X - e.End.X)
+	deltaY := Abs(e.Start.Y - e.End.Y)
+	return deltaX + deltaY
+}
+
+func (e Edge) SliceFrom(list [][]interface{}) []interface{} {
+	if e.Start.Y == e.End.Y {
+		return list[e.Start.Y][e.Start.X:e.End.X]
+	} else if e.Start.X != e.End.X {
+		panic("Edge SliceFrom method does not support diagonal edges.")
+	}
+	slice := make([]interface{}, e.Len())
+	// This is currently assuming the end is later in the matrix. We can make this
+	// more flexible later.
+	i := 0
+	for y := e.Start.Y; y <= e.End.Y; y++ {
+		slice[i] = list[y][e.Start.X]
+		i++
+	}
+	return slice
+}
