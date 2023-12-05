39b9877df44565a714c063b3d2f30b832c8bca6c
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-12-04 21:51:52 -0500

    2023: solves day 1 part 1 with nix

--- 2023/languages/nix/day_01.nix
+++ 2023/languages/nix/day_01.nix
@@ -1,14 +1,27 @@
 with builtins;
 let
-  test = true;
+  # Imports
+  lib = import <nixpkgs/lib>;
+
+  # Functions:
+  inherit (lib.strings) toInt;
+  debug = v: lib.traceIf ((getEnv "DEBUG") == "true") (deepSeq v v) v;
   lines = s: filter (x: (!isList x) && (x != "")) (split "\n" s);
+  firstDigit = s: match "[a-z]*([1-9]).*" s;
+  lastDigit = s: match ".*([1-9]).*" s;
+  sum = ints: foldl' (a: b: a + b) 0 ints;
+
+  # Test data
+  test = (getEnv "TEST") == "true";
   test_input = ''
     1abc2
     pqr3stu8vwx
     a1b2c3d4e5f
     treb7uchet
   '';
+
   inputs = lines (if test then test_input else readFile ./inputs/day-1.txt);
-  result = builtins.map (x: (match "(1)" x)) inputs;
+  digits = debug (map (x: toInt (head (firstDigit x) + head (lastDigit x))) inputs);
+  result = sum digits;
 in
-trace (head result) null
+result
