25fd0a5d1b4fbc46eea67f7a13b001e5ac1b6b2d
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-12-08 00:01:32 -0500

    2023: expand go solution template

--- 2023/languages/go/template/day{{padLeft (toString .date) "0" 2}}.go
+++ 2023/languages/go/template/day{{padLeft (toString .date) "0" 2}}.go
@@ -6,6 +6,19 @@ func init() {
 	puzzleSolutions[{{sub (atoi .date) 1}}] = day{{.date}}Solution
 }
 
+func setupDay{{.date}}() []string {
+	data := input.Lines("day-7")
+	if useTestInput {
+		data = input.Lines("test-7")
+	}
+	return data
+}
+
 func day{{.date}}Solution() {
 	fmt.Println("Day {{.date}}")
+	data := setupDay{{.date}}()
+	Solution{
+		Part1: nil,
+		Part2: nil,
+	}.Print()
 }
