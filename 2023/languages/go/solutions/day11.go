133d2117eca35c23be44474405e6a628426b5fa6
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-12-12 00:30:32 -0500

    2023: day 11 part 2 in go

--- 2023/languages/go/solutions/day11.go
+++ 2023/languages/go/solutions/day11.go
@@ -28,7 +28,6 @@ func NewExpandedSpace(data [][]rune) *lib.Grid[rune] {
 			expanded = append(expanded, slices.Clone(row))
 		}
 	}
-
 	for x := 0; x < len(expanded[0]); x++ {
 		hasGalaxy := false
 		for y := 0; y < len(expanded); y++ {
@@ -45,7 +44,6 @@ func NewExpandedSpace(data [][]rune) *lib.Grid[rune] {
 		}
 		x++
 	}
-
 	return lib.NewGrid(expanded)
 }
 
@@ -60,9 +58,14 @@ func DebugSpace(grid *lib.Grid[rune]) {
 
 func day11Solution() {
 	fmt.Println("Day 11")
-	data := setupDay11()
+	Solution{
+		Part1: day11Part1(setupDay11()),
+		Part2: day11Part2(setupDay11()),
+	}.Print()
+}
+
+func day11Part1(data [][]rune) int {
 	grid := NewExpandedSpace(data)
-	DebugSpace(grid)
 	galaxies := grid.FindAll('#')
 	sum := 0
 	for _, a := range galaxies {
@@ -70,8 +73,53 @@ func day11Solution() {
 			sum += a.Distance(b)
 		}
 	}
-	Solution{
-		Part1: sum / 2,
-		Part2: nil,
-	}.Print()
+	return sum / 2
+}
+
+func day11Part2(data [][]rune) int {
+	grid := lib.NewGrid(data)
+	emptyY, emptyX := emptySpaces(data)
+	galaxies := grid.FindAll('#')
+	sum := 0
+	for _, a := range galaxies {
+		for _, b := range galaxies {
+			sum += a.Distance(b)
+			sum += fillBetween(a.X, b.X, emptyX) + fillBetween(a.Y, b.Y, emptyY)
+		}
+	}
+	return sum / 2
+}
+
+func fillBetween(a, b int, empty map[int]bool) int {
+	mul := 1_000_000
+	count := 0
+	for k := range empty {
+		if min(a, b) < k && max(a, b) > k {
+			count++
+		}
+	}
+	return (mul - 1) * count
+}
+
+func emptySpaces(data [][]rune) (rows, cols map[int]bool) {
+	rows = make(map[int]bool)
+	cols = make(map[int]bool)
+	for y, row := range data {
+		if !slices.Contains(row, '#') {
+			rows[y] = true
+		}
+	}
+	for x := 0; x < len(data[0]); x++ {
+		hasGalaxy := false
+		for y := 0; y < len(data); y++ {
+			hasGalaxy = data[y][x] == '#'
+			if hasGalaxy {
+				break
+			}
+		}
+		if !hasGalaxy {
+			cols[x] = true
+		}
+	}
+	return
 }
