173cbe0df65523ba6556a0386d20c4d8f6eb0368
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2024-12-01 19:38:12 -0500

    refactors 2024 day 1 avoid solution package naming conflicts

--- 2024/languages/go/solutions/day01.go
+++ 2024/languages/go/solutions/day01.go
@@ -9,49 +9,49 @@ import (
 	"github.com/erikkrieg/adventofcode/2024/pkg/lib"
 )
 
-func init() {
-	puzzleSolutions[0] = day1Solution
+type Day1 struct {
+	data []string
 }
 
-func setupDay1() []string {
+func (d *Day1) Setup() {
 	data := input.Lines("day-1")
 	if useTestInput {
 		data = input.Lines("test-1")
 	}
-	return data
+	d.data = data
 }
 
-func day1Solution() {
+func (d *Day1) Solve() {
 	fmt.Println("Day 1")
-	data := setupDay1()
+	d.Setup()
 	Solution{
-		Part1: day1Part1(data),
-		Part2: day1Part2(data),
+		Part1: d.Part1(),
+		Part2: d.Part2(),
 	}.Print()
 }
 
-func day1Part1(data []string) int {
+func (d *Day1) Part1() int {
 	var distanceSum int
-	left := make([]int, len(data))
-	right := make([]int, len(data))
-	for i, d := range data {
-		fields := strings.Fields(d)
+	left := make([]int, len(d.data))
+	right := make([]int, len(d.data))
+	for i, line := range d.data {
+		fields := strings.Fields(line)
 		left[i] = lib.Atoi(fields[0])
 		right[i] = lib.Atoi(fields[1])
 	}
 	sort.Ints(left)
 	sort.Ints(right)
-	for i := 0; i < len(data); i++ {
+	for i := 0; i < len(d.data); i++ {
 		distanceSum += lib.Abs(left[i] - right[i])
 	}
 	return distanceSum
 }
 
-func day1Part2(data []string) int {
-	left := make([]int, len(data))
+func (d *Day1) Part2() int {
+	left := make([]int, len(d.data))
 	right := make(map[int]int)
-	for i, d := range data {
-		fields := strings.Fields(d)
+	for i, line := range d.data {
+		fields := strings.Fields(line)
 		left[i] = lib.Atoi(fields[0])
 		right[lib.Atoi(fields[1])] += 1
 	}
@@ -60,4 +60,8 @@ func day1Part2(data []string) int {
 		similarity += n * right[n]
 	}
 	return similarity
+}
+
+func init() {
+	puzzleSolutions[0] = (&Day1{}).Solve
 }
