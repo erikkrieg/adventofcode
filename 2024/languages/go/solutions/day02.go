08be5948d80b7a4200d33863aa01894c70647da7
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2024-12-02 14:34:12 -0500

    solves 2024 day 2 part 2 in Go

--- 2024/languages/go/solutions/day02.go
+++ 2024/languages/go/solutions/day02.go
@@ -31,28 +31,10 @@ func (d *Day2) Solve() {
 
 func (d *Day2) Part1() int {
 	totalSafeReports := 0
-reportLoop:
 	for _, report := range d.data {
-		levelDifference := 0
 		levels := strings.Fields(report)
-		var previousLevel *int
-		for _, level := range levels {
-			level := lib.Atoi(level)
-			if previousLevel != nil {
-				diff := (*previousLevel - level)
-				nextLevelDifference := levelDifference + diff
-				if lib.Abs(diff) < 1 || lib.Abs(diff) > 3 {
-					// This level is not safe because the distance between two levels
-					// is outside of the designated bounds (1-3 inclusive).
-					continue reportLoop
-				}
-				if lib.Abs(nextLevelDifference) <= lib.Abs(levelDifference) {
-					// This level is not safe because it is not always increasing or decreasing
-					continue reportLoop
-				}
-				levelDifference = nextLevelDifference
-			}
-			previousLevel = &level
+		if d.findUnsafeLevel(levels) >= 0 {
+			continue
 		}
 		totalSafeReports += 1
 	}
@@ -60,7 +42,58 @@ reportLoop:
 }
 
 func (d *Day2) Part2() int {
-	return 0
+	totalSafeReports := 0
+	for _, report := range d.data {
+		levels := strings.Fields(report)
+		firstUnsafeLevelIndex := d.findUnsafeLevel(levels)
+		if firstUnsafeLevelIndex >= 0 {
+			foundSafeReport := false
+			for i := range levels {
+				l, _ := removeIndex(levels, i)
+				foundSafeReport = d.findUnsafeLevel(l) == -1
+				if foundSafeReport {
+					break
+				}
+			}
+			if !foundSafeReport {
+				continue
+			}
+		}
+		totalSafeReports += 1
+	}
+	return totalSafeReports
+}
+
+func removeIndex[T any](slice []T, i int) ([]T, error) {
+	if i >= len(slice) || i < 0 {
+		return nil, fmt.Errorf("Index is out of bounds: %d", i)
+	}
+	s := make([]T, 0, len(slice))
+	s = append(s, slice[:i]...)
+	s = append(s, slice[i+1:]...)
+	return s, nil
+}
+
+func (d *Day2) findUnsafeLevel(levels []string) int {
+	var previousLevel, direction *int
+	for i, level := range levels {
+		level := lib.Atoi(level)
+		if previousLevel != nil {
+			diff := (*previousLevel - level)
+			diffDir := lib.Sign(diff)
+			if direction == nil {
+				direction = &diffDir
+			}
+			unsafeDiff := (lib.Abs(diff) < 1 || lib.Abs(diff) > 3)
+			unsafeDir := *direction != diffDir
+			if unsafeDiff || unsafeDir {
+				return i - 1
+			}
+
+		}
+		previousLevel = &level
+	}
+	return -1
 }
 
 func init() {
