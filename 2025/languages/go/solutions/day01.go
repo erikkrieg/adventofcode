b6f931eb7314a47528d95df4f7d6f9ccf4159edf
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2025-12-03 00:01:44 -0500

    2025(go): solution for day1 part 2

--- 2025/languages/go/solutions/day01.go
+++ 2025/languages/go/solutions/day01.go
@@ -50,13 +50,46 @@ func (d *Day1) Part1() int {
 		if pos == 0 {
 			password += 1
 		}
-		fmt.Printf("%s %d = %d\n", dir, dist, pos)
 	}
 	return password
 }
 
 func (d *Day1) Part2() int {
-	return 0
+	password := 0
+	pos := 50
+	for _, d := range d.data {
+		dir := d[:1]
+		dist, err := strconv.Atoi(d[1:])
+		if err != nil {
+			panic(err)
+		}
+		q, r := divmod(dist, 100)
+		dist = r
+		password += q
+		if dir == "L" {
+			dist *= -1
+		}
+		pos += dist
+		if pos == 0 {
+			password += 1
+		} else if pos > 99 {
+			pos %= 100
+			password += 1
+		} else if pos < 0 {
+			pos += 100
+			if pos-dist != 100 {
+				password += 1
+			}
+		}
+		fmt.Printf("%s %d %d %d\n", dir, dist, pos, password)
+	}
+	return password
+}
+
+func divmod(n, d int) (int, int) {
+	q := n / d
+	r := n % d
+	return q, r
 }
 
 func init() {
